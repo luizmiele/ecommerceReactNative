de64995edcec667904ea65ed04273c8953ec81ae
RaphaelDamico <raphaelvivarinid@gmail.com>
2024-06-21 23:07:56 -0300

    feat: cria e estiliza pagina login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,13 +1,53 @@
 import { useNavigation } from "@react-navigation/native";
 import { LoginProps, StackTypes } from "../../routes/stack";
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import { useState } from "react";
 
 export default function Login() {
     const navigation = useNavigation<StackTypes>();
-    return(
+
+    const [username, setUsername] = useState<string>('user');
+    const [password, setPassword] = useState<string>('');
+
+    return (
         <View style={styles.container}>
-            <Text>Página Login</Text>
+            <View style={styles.cardContainer}>
+                <View style={styles.headerCardContainer}>
+                    <View style={styles.headerCardContent}>
+                        <Text style={styles.title}>Enter Password</Text>
+                        <Image
+                            source={require("../../../assets/icons/botao-fechar.png")} />
+                    </View>
+                </View>
+                <View style={styles.logoContainer}>
+                    <Text style={styles.titleLogo}>PORTUGOL</Text>
+                    <Text style={styles.subtitleLogo}>Ecommerce</Text>
+                </View>
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}>Select user name:</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={username}
+                        onChangeText={(text) => setUsername(text)}
+                    />
+                    <Text style={styles.label}>Password:</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                        secureTextEntry={true}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text>0K</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -2,6 +2,81 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        backgroundColor: "#55AAAA",
+        alignItems: "center",
+        justifyContent: "center",
     },
+    cardContainer: {
+        backgroundColor: "#C3C7CB",
+        width: "80%",
+        height: "40%",
+        borderWidth: 2,
+        borderLeftColor: "#FFF",
+        borderTopColor: "#FFF",
+        paddingHorizontal: 1,
+    },
+    headerCardContainer: {
+        backgroundColor: "#0000AA",
+        width: "100%",
+    },
+    headerCardContent: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 2,
+    },
+    title: {
+        color: "#FFF",
+        fontWeight: "bold",
+    },
+    logoContainer: {
+        flex: 0.4,
+        justifyContent: "center",
+        top: "10%",
+    },
+    titleLogo: {
+        fontSize: 18,
+        fontStyle: "normal",
+        paddingHorizontal: "20%",
+    },
+    subtitleLogo: {
+        fontSize: 28,
+        fontWeight: "900",
+        bottom: "15%",
+        paddingHorizontal: "25%",
+    },
+    formContainer: {
+        paddingHorizontal: "4%",
+        top: "20%",
+    },
+    label: {
+        fontSize: 12,
+        marginBottom: 5,
+    },
+    input: {
+        width: "95%",
+        height: 20,
+        paddingHorizontal: 8,
+        borderWidth: 2,
+        backgroundColor: '#FFF',
+        borderRightColor: "#FFF",
+        borderBottomColor: "#FFF",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        top: 10,
+        paddingHorizontal: "4%"
+    },
+    btn: {
+        alignItems: "center",
+        justifyContent: "center",
+        borderRightWidth: 2,
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderBottomWidth: 3,
+        width: "30%",
+        marginHorizontal: 3,
+    }
 });
