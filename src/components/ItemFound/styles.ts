d89c171e75b523ed98d34cf4de79faebd7703a2c
Luiz Miele <luiz.miele@hotmail.com>
2024-06-23 17:45:54 -0300

    Create: Cria pagina Visualizar produtos

---
+++ src/components/ItemFound/styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create ({
+geral:{
+    backgroundColor: "#C3C7CB",
+    height: 100,
+    width: 150,
+    borderWidth: 2,
+    minHeight:'10%',
+    minWidth:'20%',
+    borderLeftColor: "#FFF",
+    borderTopColor: "#FFF",
+    paddingHorizontal: 1,
+},
+imgContainer: {
+    top: 5,
+    alignSelf: "center",
+    height: 70,
+    width: 140,
+    borderWidth: 2,
+    borderTopColor: "#000",
+    borderLeftColor: "#000",
+    borderRightColor: "#FFF",
+    borderBottomColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+},
+imagemCentro: {
+    height: 66,
+    width: 136, 
+},
+titulo: {
+    flex: 1,
+    marginLeft: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 6,
+},
+})
