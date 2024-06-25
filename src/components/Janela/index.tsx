6db3fe4df504ff5ec4d55d02c470c190be655ddc
ArturFoster <arturfosterdesouza@gmail.com>
2024-06-24 21:10:24 -0300

    add: ampulheta gif | donwload gif; update: Janela | VisualizarProduto;

--- src/components/Janela/index.tsx
+++ src/components/Janela/index.tsx
@@ -5,18 +5,21 @@ import icon1 from "../../../assets/icons/botao-fechar.png";
 import icon2 from "../../../assets/icons/botao-fechar-click.png"
 
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StackTypes } from "../../routes/stack";
 
 
 function Janela(props: propsJanela) {
-    const [apertando,setApertando]=useState(false)
+    const stack = useNavigation<StackTypes>();
+    const [apertando, setApertando] = useState(false)
 
     return (
         <View style={[styles.geral, { height: props?.height, width: props?.width }]}>
             <View style={styles.headerCardContainer}>
                 <View style={styles.headerCardContent}>
                     <Text style={styles.title}>{props.header}</Text>
-                    <Pressable onPress={props.onPress} onPressIn={()=>setApertando(true)} onPressOut={()=>setApertando(false)}>
-                        <Image source={apertando?icon2:icon1} />
+                    <Pressable onPress={()=>stack.pop()} onPressIn={() => setApertando(true)} onPressOut={() => setApertando(false)}>
+                        <Image source={apertando ? icon2 : icon1} />
                     </Pressable>
                 </View>
             </View>
--- src/screens/VisualizarProdutos/index.tsx
+++ src/screens/VisualizarProdutos/index.tsx
@@ -5,52 +5,52 @@ import Button from "../../components/Button/index";
 import ItemFound from "../../components/ItemFound/index";
 import { useEffect, useState } from "react";
 import { Modal } from "../../components/Modal";
-import { getItens } from "../../services/Api/api";
+import { getAllItens } from "../../services/Api/api";
 import { itens } from "../../types";
 import SearchBar from "../../components/SearchBar";
 
 export default function VisualizarProdutos() {
-  
-  const [lista, setLista] = useState<itens>([])
-  const [loading,setLoading] = useState<boolean>(false)
 
-  async function getAllItens() {
+  const [lista, setLista] = useState<itens>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  async function getAll() {
     setLoading(true)
     try {
-      const result = await getItens();
-      setLista(result?.data)
+      const result = await getAllItens();
+      setLista(result)
     } catch (err) {
       console.log(err)
     }
     setTimeout(() => {
       setLoading(false)
-    }, 1700);
-
+    }, 5000);
   }
+
   useEffect(() => {
-    getAllItens();
-  },[])
+    getAll();
+  }, [])
 
   return (
-    <View style={styles.container}>      
-        <Janela header="Pesquisar Produtos" height={"90%"} width={"90%"}>
-          <View style={styles.todos}>
-            <View>
-              <Text style={{textAlign:'center'}}>TODOS</Text>
-            </View>
-            
+    <View style={styles.container}>
+      <Janela header="Pesquisar Produtos" height={"90%"} width={"90%"}>
+        <View style={styles.todos}>
+          <View>
+            <Text style={{ textAlign: 'center' }}>TODOS</Text>
           </View>
-          <View style={styles.geral}>
-            <SearchBar style={styles.searchBar}/>
-            {
-            (loading?<Text>Loading...</Text>:<FlatList numColumns={(2)}
-            data={lista}
-            renderItem={({item}) => <ItemFound description={item.description} price={item.price} type={item.type} titulo={item.name} imagem={item.img}/>}
-            keyExtractor={item => item.id}        
+
+        </View>
+        <View style={styles.geral}>
+          <SearchBar style={styles.searchBar} />
+          {
+            (loading ? <View style={{marginTop:20,height:'100%'}}><Image style={{height:250,width:250,resizeMode:'contain'}}source={require('../../../assets/icons/tshell32_170.gif')}/></View>: <FlatList numColumns={(2)}
+              data={lista}
+              renderItem={({ item }) => <ItemFound description={item.description} price={item.price} type={item.type} titulo={item.name} imagem={item.img} />}
+              keyExtractor={item => item.id}
             />)
-            }
-          </View>
-        </Janela>      
+          }
+        </View>
+      </Janela>
     </View>
   );
 }
